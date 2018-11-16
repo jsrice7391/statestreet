1c86f184ccb035f6db4b03431ba6d6c8d54869f7
jsrice7391 <jsrice7391@gmail.com>
2018-11-16 00:06:57 -0500

    Added a little more logic to the filter

---
+++ README.md
@@ -0,0 +1,17 @@
+# Transaction Table
+This is a table that is built to show a large table of transactions and be able to filter on them. Each transation contains a link that goes to a different rendered component using React Router.
+
+### Key Tools Used
+- React Router
+- React 16
+- React-Bootstrap 
+- AWS S3 Bucket
+- AWS 
+- AWS CLI
+- Create-React-App
+
+### Key points
+I chose to make my application within React. When I build my React applications, I tend to organize my SRC folder such that all of my stateful componentns are within a "pages" folder and all of my stateless componenents are in a folder called "stateless." 
+
+T
+
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -18,12 +18,12 @@ export class Home extends Component {
       searched: false,
     }
   }
-  componentWillMount(){
+  componentWillMount = () =>{
           const uniqueAccount = [...new Set(data.transactions.map(item => item.accountName))];
           const uniqueType = [...new Set(data.transactions.map(item => item.transactionType))];
           this.setState({
             transactions: data.transactions,
-            accountName: uniqueAccount,
+            accountNames: uniqueAccount,
             types: uniqueType,
             columns: ["account no.", "Acount Name", "Currency", "amount", "transactiontype"]
           })
@@ -31,21 +31,30 @@ export class Home extends Component {
 
     _filterTable = (c, e) => {
       if(e.target.checked){
-        this.setState({filteredTerms: [...this.state.filteredTerms, c]}, () =>{
+        this.setState(prevstate => {
+          return {filteredTerms: [...prevstate.filteredTerms, c],filtered: true}}, () =>{
           this._renderFiltered();
         });
         }else{
-          this.setState({filteredTerms: this.state.filteredTerms.filter(term => term !== c)}, () => {
+          this.setState(prevstate => {
+            return{filteredTerms: prevstate.filteredTerms.filter(term => term !== c)}}, () => {
             this._renderFiltered();
           })
       }
       console.log(`This is C: ${c} and this is e: ${e.target.checked}`)
     }
     _renderFiltered = () =>{
-      if(this.state.filteredTerms.length > 0){
-        this.setState({
-          transactions: this.state.transactions.filter(t => this.state.filteredTerms.includes(t.accountName) || this.state.filteredTerms.includes(t.transactionType))
-        })
+        let foundAccountName = this.state.filteredTerms.some(r => this.state.accountNames.includes(r))
+        let foundType = this.state.filteredTerms.some(r => this.state.types.includes(r))
+        if(foundAccountName && foundType){
+                 this.setState({
+                   transactions: this.state.transactions.filter(t => this.state.filteredTerms.includes(t.accountName) &&this.state.filteredTerms.includes(t.transactionType))
+                 })
+          
+        }else if(foundAccountName || foundType){
+                  this.setState({
+                    transactions: this.state.transactions.filter(t => this.state.filteredTerms.includes(t.accountName) || this.state.filteredTerms.includes(t.transactionType))
+                  })
       }else{
         this.setState({transactions: data.transactions});
       }
@@ -58,11 +67,11 @@ export class Home extends Component {
         <hr/>
         <div className="menus">
         <h3>Filters</h3>
-        <Menu title="Account Name" choices={this.state.accountName} _filterTable={this._filterTable}/>
+        <Menu title="Account Name" choices={this.state.accountNames} _filterTable={this._filterTable}/>
         <Menu title="Transaction Types" choices={this.state.types} _filterTable={this._filterTable}/>
         </div>
         <div className="table">
-        <Table columns={columns} transactions={transactions} _toggleDetail={this._toggleDetail}/>
+        <Table columns={columns} transactions={transactions.slice(0,1000)} _toggleDetail={this._toggleDetail}/>
         </div>
       </div>
     );
